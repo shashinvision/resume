5bc41b2bbffbf6c594afcee82f989332bb1a5919
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-04-21 18:05:32 -0400

    ADD Method

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=['GET', 'POST'])
 def index():
-    css_bootstrap_url = url_for('static', filename = 'css/bootstrap.min.css')
-    js_bootstrap_url = url_for('static', filename = 'js/bootstrap.bundle.min.js')
-    return render_template("base.html", css_bootstrap_url = css_bootstrap_url, js_bootstrap_url = js_bootstrap_url )
+    if request.method == 'GET':
+        css_bootstrap_url = url_for('static', filename = 'css/bootstrap.min.css')
+        js_bootstrap_url = url_for('static', filename = 'js/bootstrap.bundle.min.js')
+        return render_template("base.html", css_bootstrap_url = css_bootstrap_url, js_bootstrap_url = js_bootstrap_url )
