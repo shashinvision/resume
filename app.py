78af3ad31679ea172fb180488c3ce3e98a9793a5
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-04-22 09:34:55 -0400

    ADD container size

--- app.py
+++ app.py
@@ -7,9 +7,12 @@ app = Flask(__name__)
 @app.route("/<languaje>", methods=['GET'])
 def index(languaje = 'english'):
     if request.method == 'GET':
+        style_url = url_for('static', filename = 'css/style.css')
         css_bootstrap_url = url_for('static', filename = 'css/bootstrap.min.css')
         js_bootstrap_url = url_for('static', filename = 'js/bootstrap.bundle.min.js')
+        perfil_img = url_for('static', filename = 'imgs/perfil.jpeg')
+
         
         set_languaje =  english if languaje == 'english' else spanish
 
-        return render_template("base.html", css_bootstrap_url = css_bootstrap_url, js_bootstrap_url = js_bootstrap_url, languaje=set_languaje )
+        return render_template("base.html", css_bootstrap_url = css_bootstrap_url, style_url=style_url, js_bootstrap_url = js_bootstrap_url,perfil_img=perfil_img, languaje=set_languaje )
