20c5b330a85ae819048ff2b4e29406dc3c669dd5
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-04-22 21:16:03 -0400

    ADD Skils

--- languaje.py
+++ languaje.py
@@ -5,7 +5,6 @@ english = {
     "short_title": "<b>IT ENGINEER</b> / Full Stack Developer",
     "description_question": "¿What is a <b>Full Stack</b> developer?<br>Is a developer capable of delivering a product from scratch.",
     "about_me": "Professional with 15+ years of experience in the field of computing, with performance in the following areas: software / hardware support, network / telecommunications technician and during the last years I have directed my career to the development of Full stack web software in a self-taught way, which I complement with the use of the IT engineering degree. I use the agile KANBAN methodology to improve productivity and speed of deliveries. I am constantly learning and i love teach if someone feel that can help in something new, and i love learn from people know more that me.",
-    "experience_skills":"Linux / Vim / Python / Flask / Jupyter / Pandas / JavaScript / Node / Express / VueJS / Vuex / Nuxt / Modyo / Microfrontend / PHP / CodeIgniter / Laravel / SQL (MySQL/MariaDB, SQL Server, PostgreSQL, SQLite) / MongoDB / BASH / Docker / Git / API REST / HTML5 / CSS Frameworks / AGILE / Tech Lead / Documentation",
     "skills_description":"Skills acquired in a self-taught way and later put into experience within the development areas in which I have worked.",
     "education": [
     {
@@ -27,7 +26,6 @@ spanish = {
     "short_title": "<b>Ingeniero informático</b> / Desarrollador Full Stack",
     "description_question": "¿Qué un desarrollador <b>Full Stack</b>?<br>Es un desarrollador capaz de entregar un producto desde cero.",
     "about_me": "Profesional con 15+ años de experiencia en el rubro de la informática, con desempeño en las siguientes áreas: soporte software/hardware, técnico en redes/telecomunicaciones y durante los últimos años he dirigido mi carrera al desarrollo de software web Full stack de forma autodidacta, la cual complemento con el uso de la carrera de ingeniería informática. Hago uso de la metodología ágil KANBAN para mejoras en la productividad y velocidad de entregas. Me mantengo en constante aprendizaje, me encanta enseñar si alguien siente que le puedo ayudar a aprender algo nuevo, y me encanta aprender de los que saben más.",
-    "experience_skills":"Linux / Vim / Python / Flask / Jupyter / Pandas / JavaScript / Node / Express / VueJS / Vuex / Nuxt / Modyo / Microfrontend / PHP / CodeIgniter / Laravel / SQL (MySQL/MariaDB, SQL Server, PostgreSQL, SQLite) / MongoDB / BASH / Docker / Git / API REST / HTML5 / CSS Frameworks / AGILE / Líder Técnico / Documentación",
     "skills_description":"Habilidades adquiridas de forma autodidacta y posteriormente puesta en experiencia dentro de las áreas de desarrollo en las que me he desempeñado.",
     "education": [
     {
@@ -41,5 +39,11 @@ spanish = {
         "intitution":"Centro de formación técnica, ICEL."
     },
     ]
+
+}
+
+general = {
+    "experience_skills":"Linux / Vim / Python / Flask / Jupyter / Pandas / JavaScript / Node / Express / VueJS / Vuex / Nuxt / Modyo / Microfrontend / PHP / CodeIgniter / Laravel / SQL (MySQL/MariaDB, SQL Server, PostgreSQL, SQLite) / MongoDB / BASH / Docker / Git / API REST / HTML5 / CSS Frameworks / AGILE / Tech Lead / Documentation",
+    "skills":["HTML5 / CSS3 (Frameworks / Libraries)", "Python / Flask / Pandas / Jupyter", "PHP / CodeIgniter / Laravel", "JavaScript / React / Vue / Vuex / Nuxt / Ionic", "Micro Frontend | Modyo", "Linux / MacOsx / Unix / Tmux / Windows", "GIT", "CMS (Wordpress, Joomla, Drupal, Prestashop)", "Docker / Docker Compose", "SQL (MYSQL/MariaDB, SQL Server, PostgreSQL)", "MongoDB", "Cloud: AWS / Azure", "Software architecture", "SOLID", "Figma", "Flow diagrams / UML / BPMN", "Excel", "BI - Data analyst", "Bash", "Web scraping", "Odoo ERP/CRM"],
 
 }
