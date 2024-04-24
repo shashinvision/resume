6d1d8c7318d249ebed1922eb24a92fe734770d3d
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-04-24 12:54:40 -0400

    ADD All experiencia

--- languaje.py
+++ languaje.py
@@ -36,6 +36,48 @@ english = {
             "intitution":"INDAP (Ministry of Agriculture)",
             "list": ["Full Stack Developer", "Participant in the entire life cycle of software projects", "Use GIT for control version and Docker for deploy containers", "I worked with a institutional framework for made and upgrade difference modules"]
         },
+        {
+            "title": "FRONTEND DEVELOPER",
+            "date": "2022 (Project)",
+            "intitution":"Genesys > IBM > Bank ITAU",
+            "list": ["Develop Frontend with tool Modyo | MicroFrontend", "HTML5, CSS3, Bootstrap, JavaScript, Liquid, API REST", "GIT Flow, Windows."]
+        },
+        {
+            "title": "FULL STACK WEB DEVELOPER",
+            "date": "2022 (Project)",
+            "intitution":"HUAP - emergency hospital public assistance",
+            "list": ["Support in internal system development InnHosp with LAMP Stack", "Development of data extraction system for assistance in ZKTECO biometric equipment.","GIT Flow, FTP, Linux."]
+        },
+        {
+            "title": "FULL STACK WEB DEVELOPER",
+            "date": "2021 (Project)",
+            "intitution":"AltumLab",
+            "list": ["Development with framework Laravel to Backend in PHP.", "Development with framework VueJS to Frontend in JavaScript.","Study for development with framework Vue Native on application mobile devices.", "Communication through API REST and GraphQL.", "Git Flow.", "Team leader", "I was creating development processes, implementation of Scrumban in development area, also implementation continuous integration model for projects."]
+        },
+        {
+            "title": "FULL STACK WEB DEVELOPER",
+            "date": "2019 - 2020",
+            "intitution":"Axia LTDA.",
+            "list": ["Maintenance of Linux servers with web services, BBDD and Asterisk IP telephony.","Create / edit modules and script of the Astercall application in PHP.", "Data extraction in MySQL databases (Functions, Stored Procedures).", "Bootstrap framework implementation with AdminLTE template."]
+        },
+        {
+            "title": "TECHNOLOGICAL SUPPORT, FULL STACK WEB DEVELOPER",
+            "date": "2013, 2015 - 2019",
+            "intitution":"General Directorate for Export Promotion (Ministry of Foreign Relations).",
+            "list": ["Computer software, hardware and network support.","In charge of video conferences between Chile and abroad.", "Mount Linux Web server, MySQL and Samba.", "Developer of activity log system, documentation and inventory in PHP / MySQL with CodeIgniter3 Framework (Object Oriented Programming).", "Hardening to the platform oriented to toughen the security of entry to third parties."]
+        },
+        {
+            "title": "TECHNOLOGICAL SUPPORT, WEB DEVELOPER",
+            "date": "2010 - 2011",
+            "intitution":"Ziron system.",
+            "list": ["Computer software, hardware and network support.", "Joomla CMS implementation for client intranet."]
+        },
+        {
+            "title": "TECHNOLOGICAL SUPPORT, WEB DEVELOPER",
+            "date": "2007 - 2010",
+            "intitution":"Ciber Group Contac-Center.",
+            "list": ["Computer software, hardware and network support. Minsal Help Desk, Terpel."]
+        },
     ]
     
 }
@@ -70,11 +112,53 @@ spanish = {
             "intitution":"Automovil Club de Chile",
             "list": ["Líder Técnico del equipo de desarrollo", "Desarrollador Full Stack","Administrador de la capa de datos", "Gestión de proyectos de software", "Uso de distintas metodologias y herramientas para la gestión y control de proyectos", "Participe de todo el ciclo de vida de los proyectos de software","Uso de git para versionamiento de software", "Administrador de Servidores On-presime y Cloud(AWS/AZURE).", "Micro Servicios"]
         },
-                {
-            "title": "SOFTWARE ENGINEER",
+        {
+            "title": "INGENIERO DE SOFTWARE",
             "date": "2020 - 2021, 2023 (Proyecto)",
             "intitution":"INDAP (Ministerio de Agricultura)",
             "list": ["Desarrollador Full Stack", "Participe de todo el ciclo de vida de los proyectos de software", "Uso de GIT para el control de versiones de software y Docker para el despliegue de contenedores", "Trabajo con Framework institucional para crear y mejorar direfentes modulos"]
+        },
+        {
+            "title": "DESARROLLADOR FRONTEND",
+            "date": "2022 (Proyecto)",
+            "intitution":"Genesys > IBM > Bank ITAU",
+            "list": ["Desarrollo con herramienta Modyo | MicroFrontend", "HTML5, CSS3, Bootstrap, JavaScript, Liquid, API REST", "GIT Flow, Windows."]
+        },
+        {
+            "title": "DESARROLLADOR WEB FULL STACK",
+            "date": "2022 (Proyecto)",
+            "intitution":"HUAP - Hospital de urgencia de asistencia pública",
+            "list": ["Apoyo en desarrollo de sistema interno InnHosp con LAMP Stack.", "Desarrollo de sistema de extracción de datos de asistencias en equipos biométricos ZKTECO.", "Uso de GIT, FTP, Linux."]
+        },
+        {
+            "title": "DESARROLLADOR WEB FULL STACK",
+            "date": "2021 (Proyecto)",
+            "intitution":"AltumLab",
+            "list": ["Desarrollo en framework Laravel para Backend en PHP.", "Desarrollo en framework VueJS para el Frontend en JavaScript.","Estudio para desarrollo en framework Vue Native en apliaciones móviles.", "Comunicación de API REST y GraphQL.", "Flujo de trabajo con GIT.", "Líder de equipo.", "Estuve creando procesos de desarrollo, implementación de Scrumban, al igual que implementación de modelo de integración continua para proyectos."]
+        },
+        {
+            "title": "FULL STACK WEB DEVELOPER",
+            "date": "2019 - 2020",
+            "intitution":"Axia LTDA.",
+            "list": ["Mantención de servidores Linux con servicios web, BBDD y telefonía IP Asterisk.","Crear/editar módulos y script de la aplicación Astercall en PHP.", "Extracción de datos en bases MySQL (Funciones, Procedimientos almacenados).", "Implementación de framework Bootstrap con plantilla AdminLTE."]
+        },
+        {
+            "title": "SOPORTE TECNOLOGICO, DESARROLLADOR WEB FULL STACK",
+            "date": "2013, 2015 - 2019",
+            "intitution":"Dirección General de Promoción de Exportaciones (Ministerio de Relaciones Exteriores).",
+            "list": ["Soporte informático de software, hardware y red.","Encargado de video conferencias entre Chile y el extranjero.", "Montar servidor Linux Web, MySQL y Samba.", "Desarrollador de sistema de registro de actividades, documentación e inventario en PHP/MySQL con Framework CodeIgniter3 (Programación orientada a objetos).", "Hardening a la plataforma orientado en endurecer la seguridad de ingreso a terceros."]
+        },
+        {
+            "title": "TSOPORTE TECNOLOGICO, DESARROLLADOR WEB",
+            "date": "2010 - 2011",
+            "intitution":"Ziron system.",
+            "list": ["Soporte informático de software, hardware y red.", "Implementación de CMS Joomla para intranet de cliente."]
+        },
+        {
+            "title": "TSOPORTE TECNOLOGICO, DESARROLLADOR WEB",
+            "date": "2007 - 2010",
+            "intitution":"Ciber Group Contac-Center.",
+            "list": ["Soporte informático de software, hardware y red. Mesa de Ayuda Minsal, Terpel."]
         },
     ]
 
