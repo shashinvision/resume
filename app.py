74a4eae19b7d47a9c5ca9cd33d7ce6bc02b9129a
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-04-28 18:41:56 -0400

    add year

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, url_for, request
 from language import english, spanish, generals
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -12,7 +13,8 @@ def index(language = 'english'):
         js_bootstrap_url = url_for('static', filename = 'js/bootstrap.bundle.min.js')
         perfil_img = url_for('static', filename = 'imgs/perfil.jpeg')
         icon = url_for('static', filename = 'imgs/perfil.ico')
+        current_year = datetime.now().year
 
         set_language =  english if language == 'english' else spanish
 
-        return render_template("base.html", css_bootstrap_url = css_bootstrap_url, style_url=style_url, js_bootstrap_url = js_bootstrap_url,perfil_img=perfil_img, icon=icon, language=set_language, generals=generals )
+        return render_template("base.html", css_bootstrap_url = css_bootstrap_url, style_url=style_url, js_bootstrap_url = js_bootstrap_url,perfil_img=perfil_img, icon=icon, language=set_language, generals=generals, current_year=current_year )
