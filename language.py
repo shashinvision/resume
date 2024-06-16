5eec9538c61af2d8a321ff2011015e6c658a8ce2
Felipe Mancilla <felipei.mancilla@gmail.com>
2024-06-15 20:24:51 -0400

    ADD old design and updates languajes

--- language.py
+++ language.py
@@ -6,7 +6,7 @@ english = {
     "short_title": "<b>IT ENGINEER</b> / Full Stack Developer",
     "about_me_title": "About me",
     "description_question": "¿What is a <b>Full Stack</b> developer?<br>Is a developer capable of delivering a product from scratch.",
-    "about_me": "Professional with 15+ years of experience in the field of computing, with performance in the following areas: software / hardware support, network / telecommunications technician and during the last years I have directed my career to the development of Full stack web software in a self-taught way, which I complement with the use of the IT engineering degree. I use the agile KANBAN methodology to improve productivity and speed of deliveries. I am constantly learning and i love teach if someone feel that can help in something new, and i love learn from people know more that me.",
+    "about_me": "Professional with 15+ years of experience in the field of computing, with performance in the following areas: software / hardware support, network / telecommunications technician and during the last years I have directed my career to the development of Full stack web software in a self-taught way, which I complement with the use of the IT engineering degree. I use the agile KANBAN methodology to improve productivity and speed of deliveries. I am constantly learning, I love teach if someone feel that can help in something new, and I like sharing knowledge with my colleagues.",
     "skills_description":"Skills acquired in a self-taught way and later put into experience within the development areas in which I have worked.",
     "education": [
     {
@@ -93,7 +93,7 @@ spanish = {
     "short_title": "<b>Ingeniero informático</b> / Desarrollador Full Stack",
     "about_me_title": "Acerca de mi",
     "description_question": "¿Qué un desarrollador <b>Full Stack</b>?<br>Es un desarrollador capaz de entregar un producto desde cero.",
-    "about_me": "Profesional con 15+ años de experiencia en el rubro de la informática, con desempeño en las siguientes áreas: soporte software/hardware, técnico en redes/telecomunicaciones y durante los últimos años he dirigido mi carrera al desarrollo de software web Full stack de forma autodidacta, la cual complemento con el uso de la carrera de ingeniería informática. Hago uso de la metodología ágil KANBAN para mejoras en la productividad y velocidad de entregas. Me mantengo en constante aprendizaje, me encanta enseñar si alguien siente que le puedo ayudar a aprender algo nuevo, y me encanta aprender de los que saben más.",
+    "about_me": "Profesional con 15+ años de experiencia en el rubro de la informática, con desempeño en las siguientes áreas: soporte software/hardware, técnico en redes/telecomunicaciones y durante los últimos años he dirigido mi carrera al desarrollo de software web Full stack de forma autodidacta, la cual complemento con el uso de la carrera de ingeniería informática. Hago uso de la metodología ágil KANBAN para mejoras en la productividad y velocidad de entregas. Me mantengo en constante aprendizaje, me gusta compartir conocimiento con mis colegas.",
     "skills_description":"Habilidades adquiridas de forma autodidacta y posteriormente puesta en experiencia dentro de las áreas de desarrollo en las que me he desempeñado.",
     "education": [
     {
