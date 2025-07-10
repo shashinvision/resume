81b8e53ce0a547275e62e4980f4bd4798406c96f
Felipe Mancilla <felipei.mancilla@gmail.com>
2025-07-10 12:11:23 -0400

    update

--- db/database.py
+++ db/database.py
@@ -1,8 +1,12 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))  # carpeta db
+DB_PATH = os.path.abspath(os.path.join(BASE_DIR, "..", "sql_app.db"))
+
+SQLALCHEMY_DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
